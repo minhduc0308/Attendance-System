0e9f85e925aec6cdc76dd59490754eea5f7d25ea
DukeHLFU <156291604+DukeHLFU@users.noreply.github.com>
2024-01-30 07:41:23 +0700

    Update ControllerLecture.java

--- TestDemo/src/java/controller/ControllerLecture.java
+++ TestDemo/src/java/controller/ControllerLecture.java
@@ -29,7 +29,7 @@ public class ControllerLecture extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try {
+        try (PrintWriter out = response.getWriter()) {
             DAOLecture dao = new DAOLecture();
             String service = request.getParameter("service");
 
@@ -60,7 +60,7 @@ public class ControllerLecture extends HttpServlet {
                 if (submit == null) {
                     Vector<Lecture> vector = dao.getData("select * from lectures");
                     request.setAttribute("data", vector);
-                    dispatch(request, response, "/jsp/InsertLecture.jsp");
+                    dispath(request, response, "/jsp/InsertLecture.jsp");
                 } else {
                     String Lecid = request.getParameter("id");
                     String mail = request.getParameter("email");
@@ -80,33 +80,91 @@ public class ControllerLecture extends HttpServlet {
                 }
             }
 
-            // ... (similar corrections for other cases)
-            
-        } catch (Exception e) {
-            e.printStackTrace();
+            if (service.equals("deleteLecture")) {
+                int cid = Integer.parseInt(request.getParameter("cid"));
+                int n = dao.deleteLecture(cid);
+                String st = "delete success";
+                if (n == 0) {
+                    st = "can't delete because ...";
+                }
+                response.sendRedirect("SemesterURL?message=" + st);
+            }
+
+            if(service.equals("updateLecture")){
+                String update = request.getParameter("submit");
+                if(update==null){
+                    BigInteger cid = new BigInteger(request.getParameter("cid"));
+                    Vector<Lecture> vector = dao.getData("select * from orders where idLecture="+cid);
+                    request.setAttribute("data", vector);
+                    dispath(request, response, "/jsp/UpdateLecture.jsp");
+                }else{
+                      String Lecid = request.getParameter("id");
+                    String mail = request.getParameter("email");
+                    String name = request.getParameter("lecName");
+                    String phone = request.getParameter("Phone");
+                    String address = request.getParameter("Address");
+                    String DOB = request.getParameter("DoB");
+
+                    BigInteger id = new BigInteger(Lecid);
+                    int Phone = Integer.parseInt(phone);
+                    Date dateOfBirth = Date.valueOf(DOB);
+
+                    Lecture pro = new Lecture(id, mail, name, Phone, address, dateOfBirth);
+                    int n = dao.updateLecture(pro);
+                    String st = "";
+                    response.sendRedirect("SemesterURL?message="+st);
+                 }
+            }
         }
+
     }
 
-    void dispatch(HttpServletRequest request, HttpServletResponse response, String page)
+    void dispath(HttpServletRequest request,
+            HttpServletResponse response, String page)
             throws ServletException, IOException {
-        RequestDispatcher dispatcher = request.getRequestDispatcher(page);
-        dispatcher.forward(request, response);
+        RequestDispatcher dispath
+                = request.getRequestDispatcher(page);
+        //display
+        dispath.forward(request, response);
     }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
 
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }
+    }// </editor-fold>
+
 }
